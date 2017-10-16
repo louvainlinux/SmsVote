fb28eb9c3ab80ea856087de1a201ebf039b336af
Pierre <pierre.ortegat@gmail.com>
2017-10-16 10:08:33 +0200

    improvements

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ isStrict = False
 allowedEntry = ['coucou']
 
 # to copy the generated html file to the remote server
-local = "./out/index.html"
 remote = "/var/www/louvainlinux/public/sms-vote/"
 rsakey = "/home/pierre/.ssh/id_rsa2"
