6b1d8d0e8ee076d2f43943dfeb03cc1725b3bf1a
Pierre <pierre.ortegat@gmail.com>
2017-10-16 08:57:37 +0200

    removed useless config

--- config.py
+++ config.py
@@ -6,9 +6,6 @@ table = 'velo'
 isStrict = False
 allowedEntry = ['coucou']
 
-# destination of the index.html
-dest = '24h_JuIkfefogs1.html'
-
 # to copy the generated html file to the remote server
 local = "./out/index.html"
 remote = "/var/www/louvainlinux/public/sms-vote/"
