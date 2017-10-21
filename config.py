9ce9957a10b7dbd10fdcf11f98c1eeb4f77783ea
Pierre <pierre.ortegat@gmail.com>
2017-10-21 09:26:09 +0200

    config 24h cse

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 # to querry the db
     # the list of all the votes, with their caracteristics
 vote = [
-        {"prefix" : "velo", "filtered" : False, "allowedEntry" : ["coucou", "salut"]},
-        {"prefix" : "talent", "filtered": False, "allowedEntry": []},
-        {"prefix" : "lln", "filtered" : True, "allowedEntry": ["haut", "bas"]}
+        {"prefix" : "folklo", "filtered": True, "allowedEntry": ["1","4","6","8","9","11","12","13","14","15","16","17","18","20","21","22","25","27","28","30","31","32","33","34","36","37","39","42","43","45","50","53","57","59","61","62","64","66","67","68","70","71","74"]}
         ]
 
 # to copy the generated html file to the remote server
